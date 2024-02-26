ab47b3d45a1e868aa4a6473eea77a796e05b4588
chifahe <673187135@qq.com>
2024-02-26 19:53:04 +0800

    Fixed problem of changing value of lpCmdline.

--- wiewer.cpp
+++ wiewer.cpp
@@ -1,6 +1,4 @@
-﻿// HACK: I found that I changed value of lpCmdLine, it's bad and I have to fix it.
-
-#include "framework.h"
+﻿#include "framework.h"
 #include "wiewer.h"
 #include <windows.h>
 #include <strsafe.h>
@@ -33,146 +31,161 @@ static wil::com_ptr<ICoreWebView2Controller> webviewController;
 static wil::com_ptr<ICoreWebView2> webviewWindow;
 
 int CALLBACK wWinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ PWSTR     lpCmdLine,
-    _In_ int       nCmdShow
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ PWSTR     lpCmdLine,
+	_In_ int       nCmdShow
 )
 {
-    WNDCLASSEX wcex;
-    wcex.cbSize = sizeof(WNDCLASSEX);
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = szWindowClass;
-    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 
-    if (!RegisterClassEx(&wcex))
-    {
-        MessageBox(NULL,
-            _T("Call to RegisterClassEx failed!"),
-            _T("Windows Desktop Guided Tour"),
-            NULL);
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Windows Desktop Guided Tour"),
+			NULL);
 
-        return 1;
-    }
-    // Store instance handle in our global variable
-    hInst = hInstance;
+		return 1;
+	}
 
-    // TODO: Add a separate wchar with the same value as lpCmdLine.
+	// Store instance handle in our global variable
+	hInst = hInstance;
 
-	TCHAR* szTitle; // The text that appears in the title bar.
-    // If lpCmdLine is empty, a memory read error will occur during CreateWindow.
+	TCHAR* szTitle;									// The text that appears in the title bar.
+	wchar_t url_file_path[268] = TEXT("file:///");	// File path used in WebView2.
+
+	// If lpCmdLine is empty, a memory read error will occur during CreateWindow.
 	if (wcslen(lpCmdLine) != 0) {
-        // Set title as file name.
-        szTitle = wcsrchr(lpCmdLine, L'\\') + 1;    // This is a read operation, will not change lpCmdLine.
-        // HACK: Is this unsafe?
-    }
-    else
-    {
-        // The default text that appears in the title bar.
-        szTitle = (LPWSTR)_T("wiewer - A simple media viewer based on WebView2");
-    }
+		// Set title as file name.
+		szTitle = wcsrchr(lpCmdLine, L'\\') + 1;	// This is a read operation, will not change lpCmdLine.
 
-    // The parameters to CreateWindow explained:
-    // szWindowClass: the name of the application
-    // szTitle: the text that appears in the title bar
-    // WS_OVERLAPPEDWINDOW: the type of window to create
-    // CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
-    // 500, 100: initial size (width, length)
-    // NULL: the parent of this window
-    // NULL: this application does not have a menu bar
-    // hInstance: the first parameter from WinMain
-    // NULL: not used in this application
-    HWND hWnd = CreateWindow(
-        szWindowClass,
-        szTitle,
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT,
-        680, 600,
-        NULL,
-        NULL,
-        hInstance,
-        NULL
-    );
+		wcscat_s(url_file_path, lpCmdLine);
+		while (true)
+		{
+			int p = 0;
+			if (url_file_path[p] == '\0') {
+				break;
+				if (url_file_path[p] == '\\') {
+					url_file_path[p] = '/';
+				}
+			}
+		}
+	}
+	else
+	{
+		// The default text that appears in the title bar.
+		szTitle = (LPWSTR)_T("wiewer - A simple media viewer based on WebView2");
 
-    // TODO: Make window's initial size fit the file.
+		// It will show an empty page.
+		url_file_path[0] = '\0';	// HACK: Will this remain in memory?
+	}
 
-    if (!hWnd)
-    {
-        MessageBox(NULL,
-            _T("Call to CreateWindow failed!"),
-            _T("Windows Desktop Guided Tour"),
-            NULL);
+	// The parameters to CreateWindow explained:
+	// szWindowClass: the name of the application
+	// szTitle: the text that appears in the title bar
+	// WS_OVERLAPPEDWINDOW: the type of window to create
+	// CW_USEDEFAULT, CW_USEDEFAULT: initial position (x, y)
+	// 500, 100: initial size (width, length)
+	// NULL: the parent of this window
+	// NULL: this application does not have a menu bar
+	// hInstance: the first parameter from WinMain
+	// NULL: not used in this application
+	HWND hWnd = CreateWindow(
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		680, 600,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
 
-        return 1;
-    }
+	// TODO: Make window's initial size fit the file.
 
-    // The parameters to ShowWindow explained:
-    // hWnd: the value returned from CreateWindow
-    // nCmdShow: the fourth parameter from WinMain
-    ShowWindow(hWnd,
-        nCmdShow);
+	if (!hWnd)
+	{
+		MessageBox(NULL,
+			_T("Call to CreateWindow failed!"),
+			_T("Windows Desktop Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	// The parameters to ShowWindow explained:
+	// hWnd: the value returned from CreateWindow
+	// nCmdShow: the fourth parameter from WinMain
+	ShowWindow(hWnd,
+		nCmdShow);
 	UpdateWindow(hWnd);
 
-    // <-- WebView2 sample code starts here -->
+	// <-- WebView2 sample code starts here -->
 
-    HRESULT res = CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
-        Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
-            [hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
-                //MessageBoxA(hWnd, "createView", "", NULL);
-                // Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
-                env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
-                    [hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
-                        if (controller != nullptr) {
-                            webviewController = controller;
-                            webviewController->get_CoreWebView2(&webviewWindow);
-                        }
+	HRESULT res = CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr,
+		Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(
+			[url_file_path,hWnd](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+				//MessageBoxA(hWnd, "createView", "", NULL);
+				// Create a CoreWebView2Controller and get the associated CoreWebView2 whose parent is the main window hWnd
+				env->CreateCoreWebView2Controller(hWnd, Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(
+					[url_file_path,hWnd](HRESULT result, ICoreWebView2Controller* controller) -> HRESULT {
+						if (controller != nullptr) {
+							webviewController = controller;
+							webviewController->get_CoreWebView2(&webviewWindow);
+						}
 
-                        // Add a few settings for the webview
-                        // The demo step is redundant since the values are the default settings
-                        ICoreWebView2Settings* Settings;
-                        webviewWindow->get_Settings(&Settings);
-                        Settings->put_IsScriptEnabled(TRUE);
-                        Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
-                        Settings->put_IsWebMessageEnabled(TRUE);
+						// Add a few settings for the webview
+						// The demo step is redundant since the values are the default settings
+						ICoreWebView2Settings* Settings;
+						webviewWindow->get_Settings(&Settings);
+						Settings->put_IsScriptEnabled(TRUE);
+						Settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+						Settings->put_IsWebMessageEnabled(TRUE);
 
-                        // Resize WebView to fit the bounds of the parent window
-                        RECT bounds;
-                        GetClientRect(hWnd, &bounds);
-                        webviewController->put_Bounds(bounds);
+						// Resize WebView to fit the bounds of the parent window
+						RECT bounds;
+						GetClientRect(hWnd, &bounds);
+						webviewController->put_Bounds(bounds);
 
-                        // Schedule an async task to navigate
-						HRESULT res = webviewWindow->Navigate(L"");
-                        std::string sres = std::to_string(res).c_str();
+						// Schedule an async task to navigate
+						HRESULT res = webviewWindow->Navigate(url_file_path);
+						std::string sres = std::to_string(res).c_str();
 
-                        // Step 4 - Navigation events
+						// Step 4 - Navigation events
 
-                        // Step 5 - Scripting
+						// Step 5 - Scripting
 
-                        // Step 6 - Communication between host and web content
+						// Step 6 - Communication between host and web content
 
-                        return S_OK;
-                    }).Get());
-                return S_OK;
-            }).Get());
-    // <-- WebView2 sample code ends here -->
+						return S_OK;
+					}).Get());
+				return S_OK;
+			}).Get());
+	// <-- WebView2 sample code ends here -->
 
-    // Main message loop:
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	// Main message loop:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-    return (int)msg.wParam;
+	return (int)msg.wParam;
 }
 
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
@@ -182,21 +195,21 @@ int CALLBACK wWinMain(
 //  WM_DESTROY  - post a quit message and return
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_SIZE:
-        if (webviewController != nullptr) {
-            RECT bounds;
-            GetClientRect(hWnd, &bounds);
-            webviewController->put_Bounds(bounds);
-        };
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-        break;
-    }
-    return 0;
+	switch (message)
+	{
+	case WM_SIZE:
+		if (webviewController != nullptr) {
+			RECT bounds;
+			GetClientRect(hWnd, &bounds);
+			webviewController->put_Bounds(bounds);
+		};
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+		break;
+	}
+	return 0;
 }
